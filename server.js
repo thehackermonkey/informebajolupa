b2ea6507f9915332241cc5e60a12939e5bee3665
regenhans <hnsdream@gmail.com>
2016-09-14 15:14:02 -0500

    heroku port

--- server.js
+++ server.js
@@ -50,6 +50,6 @@ app.get('/guadalajara', function (req, res) {
 });
 
 
-app.listen(3000, function () {
+app.listen(process.env.PORT || 5000, function () {
   console.log('Example app listening on port 3000!');
 });
