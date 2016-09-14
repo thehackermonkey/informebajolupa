f69d4691e1a10d116523be307b3514e6d410d28e
regenhans <hnsdream@gmail.com>
2016-09-14 18:53:55 -0500

    logos social and swag

--- server.js
+++ server.js
@@ -3,35 +3,36 @@ var app = express();
 
 ///// GOOGLE SHEET STUFF
 var tabletop = require('tabletop');
-var googleURL= "https://docs.google.com/spreadsheets/u/1/d/19F4xqdTfV1Xse0DF0g3lLFrLFkV43agtCJNhBAu9HgM/pubhtml";
+var gdlURL= "https://docs.google.com/spreadsheets/u/1/d/19F4xqdTfV1Xse0DF0g3lLFrLFkV43agtCJNhBAu9HgM/pubhtml";
+var zapURL= "https://docs.google.com/spreadsheets/u/1/d/1CMTFGw3eTFDtoEaz8QAH4u9y5H3wKaUMt29K5YAoZhY/pubhtml?gid=0&single=true";
 var sheetdata, zapdata;
 
 //Get spreadsheet data
 function checkGDL() {
-  tabletop.init( { key: googleURL,
+  tabletop.init( { key: gdlURL,
                    callback: returnGDL,
                    simpleSheet: true } )
 };
-// function checkZPN() {
-//   tabletop.init( { key: googleURL,
-//                    callback: returnZPN,
-//                    simpleSheet: true } )
-// };
+function checkZPN() {
+  tabletop.init( { key: zapURL,
+                   callback: returnZPN,
+                   simpleSheet: true } )
+};
 
 
 function returnGDL(data, tabletop) {
-    console.log(data);
     sheetdata = data;
 }
-// function returnZPN(data, tabletop){
-//     zapdata = data;
-// }
+function returnZPN(data, tabletop){
+    zapdata = data;
+    console.log(data);
+}
 
 //look for changes on the spreadsheet and keep looking every minute
 checkGDL();
-// checkZPN();
+checkZPN();
 setInterval(checkGDL, 60000);
-// setInterval(checkZPN, 180000);
+setInterval(checkZPN, 180000);
 ///END GOOGLE SHEET
 
 
@@ -43,12 +44,18 @@ app.get('/', function (req, res) {
 });
 
 app.get('/zapopan', function (req, res) {
-  res.render('zapopan', {data: sheetdata});
+  res.render('zapopan', {data: zapdata});
 });
 
 app.get('/guadalajara', function (req, res) {
   res.render('guadalajara', {data: sheetdata});
 });
+
+app.get('/acerca', function (req, res) {
+  res.render('acerca');
+});
+
+
 
 
 app.listen(process.env.PORT || 5000, function () {
